3f7a40178a7adbb55c8dc105d178dc8ee2543711
Adrian Berisha <xxa_xxb_adrian@hotmail.de>
2017-03-30 11:36:39 +0200

    Update

--- TestP2PNetwork/app/src/main/java/de/a_berisha/testp2pnetwork/MainActivity.java
+++ TestP2PNetwork/app/src/main/java/de/a_berisha/testp2pnetwork/MainActivity.java
@@ -24,6 +24,7 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -33,91 +34,50 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnCreate;
-    Button btnSearch;
-    TextView textView;
-    EditText serviceName;
+    private Button btnSearch;
+    private TextView textView;
 
-    WifiP2pManager.Channel gameChannel;
-    WifiP2pManager manager;
+    private WifiP2pManager.Channel gameChannel;
+    private WifiP2pManager manager;
+    private Receiver receiver;
 
-    WifiP2pDnsSdServiceRequest serviceRequest;
-    WifiP2pDnsSdServiceInfo wifiServiceInfo;
+    private IntentFilter filter = new IntentFilter();
 
-    private final IntentFilter filter = new IntentFilter();
-    Receiver receiver;
-
-    MainActivity activity = this;
-
-    final static String serviceType = "_tennis._tcp";
-
-    Collection<WifiP2pDevice> wifiDeviceList;
+    private Collection<WifiP2pDevice> wifiDeviceList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialize Views
+        textView = (TextView) findViewById(R.id.textV);
+        btnSearch = (Button) findViewById(R.id.buttonSearch);
+
         //Add Actions if Peers is changed
         filter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
         filter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        filter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        filter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
-        //Initialize WifiP2pManager and Channel
+
+        //Initialize WifiP2pManager, Channel and Receiver
         manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         gameChannel = manager.initialize(this, getMainLooper(), null);
-
-        //Initialize BroadCastReceiver
         receiver = new Receiver(manager, gameChannel, this);
 
-        //Initialize Views
-        textView = (TextView)findViewById(R.id.textView);
-        btnCreate = (Button) findViewById(R.id.buttonCreate);
-        btnSearch = (Button) findViewById(R.id.buttonSearch);
-        serviceName = (EditText) findViewById(R.id.serviceName);
 
 
-        //Button onClickListener
-        btnCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-//                Map<String, String> txtMap = new HashMap<>();
-//                txtMap.put("port", "9876");
-//
-//                wifiServiceInfo = WifiP2pDnsSdServiceInfo.newInstance(serviceName.getText().toString(), serviceType,txtMap);
-//
-//                manager.addLocalService(gameChannel, wifiServiceInfo, new WifiP2pManager.ActionListener() {
-//                    @Override
-//                    public void onSuccess() {
-//                        logAll("Create Local Service successfully.");
-//                    }
-//
-//                    @Override
-//                    public void onFailure(int reason) {
-//                        logAll("Creating the Local Service failed.");
-//                    }
-//                });
-
-                searchP2PDevices();
-
-            }
-        });
-
+       //Button onClickListener
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //searchForServices();
+                searchP2PDevices();
             }
         });
 
     }
 
-    // This Method log the Messages in the Console
-    // and on the TextView in the Application
-    public void logAll(String log) {
-        textView.append(log+"\n");
-        System.out.println(log);
-    }
     public void searchP2PDevices(){
 
         // Discover for Peer to Peer Devices
@@ -129,13 +89,63 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int reason) {
-                logAll("Discover Peers Fails.");
+                logAll("Discover for peers fails." + codeErrorMessage(reason));
             }
         });
     }
 
+    // Connect to the Device
+    public void connect(WifiP2pDevice dev) {
+        WifiP2pConfig config = new WifiP2pConfig();
+        config.deviceAddress = dev.deviceAddress;
 
-    /*
+        // Connect to the Device
+        manager.connect(gameChannel, config, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                logAll("Connecting successful.");
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                logAll("Connecting failed."+ codeErrorMessage(reason));
+            }
+        });
+
+    }
+
+    // This Method log the Messages in the Console
+    // and in the TextView of the Application
+    public void logAll(String log) {
+        textView.append(log+'\n');
+        System.out.println(log);
+    }
+    public String codeErrorMessage(int code){
+        String error;
+        switch(code){
+            case 0: error = " (Error)"; break;
+            case 1: error = " (P2P not supported)"; break;
+            case 2: error = " (Busy)"; break;
+            default: error = ""; break;
+        }
+        return error;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, filter);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+
+    public void setWifiDeviceList(Collection<WifiP2pDevice> dev){ this.wifiDeviceList = dev; }
+
+        /*
     public void searchForServices(){
         WifiP2pManager.DnsSdTxtRecordListener txtRecordListener = new WifiP2pManager.DnsSdTxtRecordListener() {
             @Override
@@ -189,118 +199,4 @@ public class MainActivity extends AppCompatActivity {
     }
     */
 
-    public void connect(WifiP2pDevice dev) {
-        WifiP2pConfig config = new WifiP2pConfig();
-        config.deviceAddress = dev.deviceAddress;
-
-        // Connect to the Device
-        manager.connect(gameChannel, config, new WifiP2pManager.ActionListener() {
-            @Override
-            public void onSuccess() {
-                logAll("Connecting successful");
-
-                // Create Group
-                manager.createGroup(gameChannel, new WifiP2pManager.ActionListener() {
-                    @Override
-                    public void onSuccess() {
-                        logAll("Created Group successful.");
-
-                        // Request Group Infos
-                        manager.requestGroupInfo(gameChannel, new WifiP2pManager.GroupInfoListener() {
-                            @Override
-                            public void onGroupInfoAvailable(WifiP2pGroup group) {
-                                // Show Network-Name of this Group
-                                logAll("Network-Name: " + group.getNetworkName());
-
-                                // Show all Clients
-                                Collection<WifiP2pDevice> clientList =  group.getClientList();
-                                while(clientList.iterator().hasNext()){
-                                    logAll("Client-Adr: " + clientList.iterator().next());
-                                }
-
-
-                                // Do this only with more then one Device
-                                if(clientList.size()>1) {
-                                    //Group Owner should send Data to another Device
-                                    if (group.isGroupOwner()) {
-                                        logAll("You Are The Group Owner");
-
-                                    }
-                                }
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onFailure(int reason) {
-                        logAll("Creating Group failed.");
-                    }
-                });
-            }
-
-            @Override
-            public void onFailure(int reason) {
-                logAll("Connecting failed");
-            }
-        });
-    }
-
-
-
-
- /*   public WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
-        @Override
-        public void onPeersAvailable(WifiP2pDeviceList peers) {
-            wifiDeviceList = peers.getDeviceList();
-
-            WifiP2pDevice device = null;
-            while(wifiDeviceList.iterator().hasNext()){
-                device = wifiDeviceList.iterator().next();
-                System.out.println("Mac: " + device.deviceAddress + "Name: " + device.deviceName+'\n');
-            }
-            WifiP2pConfig config = new WifiP2pConfig();
-            config.deviceAddress = device.deviceAddress;
-
-            manager.connect(gameChannel, config, new WifiP2pManager.ActionListener() {
-                @Override
-                public void onSuccess() {
-                    Toast.makeText(activity, "Connection Success", Toast.LENGTH_SHORT);
-                }
-
-                @Override
-                public void onFailure(int reason) {
-                    Toast.makeText(activity, "Connection Fails", Toast.LENGTH_SHORT);
-                }
-            });
-        }
-    };*/
-
-
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        registerReceiver(receiver, filter);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(receiver);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        if(wifiServiceInfo!=null)
-            manager.removeLocalService(gameChannel, wifiServiceInfo, null);
-        if(serviceRequest != null)
-            manager.removeServiceRequest(gameChannel, serviceRequest, null);
-    }
-
-    public void setWifiDeviceList(Collection<WifiP2pDevice> dev){
-        this.wifiDeviceList = dev;
-    }
 }
